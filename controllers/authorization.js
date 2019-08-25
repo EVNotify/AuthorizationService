9a9f90fcbb7d6aff358f74def8c3c750f5d21634
GPlay97 <GPlay97@users.noreply.github.com>
2019-08-25 16:14:28 +0200

    first implementation to retrieve key

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -1,17 +1,19 @@
 const asyncHandler = require('../utils/asyncHandler');
 const KeyModel = require('../models/Key');
 
-const getQuota = asyncHandler(async (req, res, next) => {
-    res.json(await KeyModel.find().select(['-_id', 'key']));
+const getKey = asyncHandler(async (req, res, next) => {
+    res.json(await KeyModel.findOne({
+        key: req.authKey
+    }).select(['-_id']));
 });
 
-const useQuota = asyncHandler(async (req, res, next) => {
+const useKey = asyncHandler(async (req, res, next) => {
     res.json(await KeyModel.create({
         key: 'Test'
     }));
 });
 
 module.exports = {
-    getQuota,
-    useQuota
+    getKey,
+    useKey
 };
--- routes/authorization.js
+++ routes/authorization.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const keyUtils = require('../utils/key');
+
 const authorizationController = require('../controllers/authorization');
 
-router.get('/', authorizationController.getQuota);
-router.post('/', authorizationController.useQuota);
+const attachKey = (req, _res, next) => {
+    req.authKey = keyUtils.extractKey(req);
+    next();
+};
+
+router.get('/', attachKey, authorizationController.getKey);
+router.post('/', attachKey, authorizationController.useKey);
 
 module.exports = router;
