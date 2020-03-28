4864277989a1a7f741d6fe7419724d1dc1efcba3
GPlay97 <GPlay97@users.noreply.github.com>
2020-03-28 01:31:57 +0100

    preparation of first advanced handling check

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -25,8 +25,53 @@ const useKey = asyncHandler(async (req, res, next) => {
     const features = Array.isArray(key.features) ? key.features : [];
 
     if (req.body.referer == null || typeof req.body.referer.method !== 'string' || typeof req.body.referer.path !== 'string') return next(errors.FORBIDDEN);
-    // check if feature is allowed
-    if (!(features.some((feature) => feature.path === req.body.referer.path && feature.method === req.body.referer.method))) return next(errors.FORBIDDEN);
+    // check if dynamic URL parameters have been passed to the parsed request URL
+    if (req.body.referer.params != null && typeof req.body.referer.params === 'object' && Object.keys(req.body.referer.params).length) {
+        if (!(features.some((feature) => {
+            let isValid = false;
+            const originalParts = req.body.referer.path.split('/');
+            const featureParts = feature.path.split('/');
+
+            /*
+            Experimental new way to check
+            GET /users/Max/log/123
+
+            (GET /users/:username/log/:id)
+
+            GET /users/:username/station/:id
+            */
+            originalParts.forEach((originalPart, currentIdX) => {
+                const featurePart = featureParts[currentIdX];
+
+                if (featurePart) {
+                    if (featurePart === originalPart) {
+                        // good
+                    } else if (featurePart.includes(':') && Object.keys(req.body.referer.params).includes(featurePart.substr(1)) && req.body.referer.params[featurePart.substr(1)] === originalPart) {
+                        // good
+                    }
+                }
+            });
+
+            /**
+                GET /users/:username/log/:id
+
+                GET /users/:username/station/:id
+
+                This check works for parameters check, but does not care about the rest (critical!)
+             */
+            
+            // // ensure that path includes dynamic parameter identifier
+            // if (feature.path.includes(':')) {
+            //     // ensure that every dynamic parameter is used in our request
+            //     return Object.keys(req.body.referer.params).every((key) => feature.path.includes(`/:${key}`));
+            // }
+            return isValid;
+        }))) return next(errors.FORBIDDEN);
+    } else {
+        // check if feature is allowed
+        if (!(features.some((feature) => feature.path === req.body.referer.path && feature.method === req.body.referer.method))) return next(errors.FORBIDDEN);
+    }
+
     // quota / usage
     if ((++key.usage || 0) > (key.quota || 0)) {
         res.setHeader('Retry-After', 10);
