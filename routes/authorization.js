6a24ecf05eea5e6ce78f75e8da08d623b9cc8bd8
GPlay97 <GPlay97@users.noreply.github.com>
2020-06-26 01:21:38 +0200

    add documentation to create api key

--- routes/authorization.js
+++ routes/authorization.js
@@ -11,7 +11,7 @@ const attachKey = (req, _res, next) => {
 };
 
 /**
- * @api {get} /authorization/:key Retrieve authorization information
+ * @api {get} /authorization/:key Retrieve API-Key information
  * @apiName GetAuthorizationKey
  * @apiGroup Authorization
  * 
@@ -49,6 +49,37 @@ const attachKey = (req, _res, next) => {
  *  }
  */
 router.get('/:key', attachKey, authorizationController.getKey);
+/**
+ * @api {post} /authorization/ Create new API-Key
+ * @apiName CreateAuthorizationKey
+ * @apiGroup Authorization
+ * 
+ * @apiDescription In order to be able to communicate with the API you need a valid API key.
+ * To obtain a default API key, you can use this function. However, for custom API quota, specific roles / features, a manual contact is required (info<at>evnotify.de).
+ * 
+ * @apiParam  {String[]} scopes the AKey list where API key should be used for
+ * 
+ * @apiSuccess {String} key The created authorization API key
+ * @apiSuccess {Number} quota The amount of requests allowed in a month
+ * @apiSuccess {Number} usage The amount of already used quota
+ * @apiSuccess {String} hostname The hostname where key is valid for (* for no limitation)
+ * @apiSuccess {String[]} scopes List of AKeys where the API key can be used for
+ * @apiSuccess {Object[]} features List of features from HTTP routes / actions where the API key can be used for
+ * 
+ * @apiSuccessExample Authorization key information
+ *  HTTP/1.1 200 OK
+ *  {
+ *      "key": "TestKey",
+ *      "quota": "100",
+ *      "usage": "12",
+ *      "hostname": "example.com",
+ *      "scopes": ["123456"],
+ *      "features": [{
+ *          method: 'GET',
+ *          path: '/some/action'
+ *      }]
+ *  }
+ */
 router.post('/', authorizationController.createKey);
 router.post('/:key', attachKey, authorizationController.useKey);
 
