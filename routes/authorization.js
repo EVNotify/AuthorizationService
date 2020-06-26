e6bdd2d8977275b9d78db9bfbac02e28f1c119c5
GPlay97 <GPlay97@users.noreply.github.com>
2020-06-26 02:27:28 +0200

    add documentation for using the key

--- routes/authorization.js
+++ routes/authorization.js
@@ -66,7 +66,7 @@ router.get('/:key', attachKey, authorizationController.getKey);
  * @apiSuccess {String[]} scopes List of AKeys where the API key can be used for
  * @apiSuccess {Object[]} features List of features from HTTP routes / actions where the API key can be used for
  * 
- * @apiSuccessExample Authorization key information
+ * @apiSuccessExample API Key created
  *  HTTP/1.1 200 OK
  *  {
  *      "key": "TestKey",
@@ -81,6 +81,47 @@ router.get('/:key', attachKey, authorizationController.getKey);
  *  }
  */
 router.post('/', authorizationController.createKey);
+/**
+ * @api {post} /authorization/:key Authorize and track usage of API-Key
+ * @apiName UseAuthorizationKey
+ * @apiGroup Authorization
+ * 
+ * @apiDescription In order to be able to communicate with the API you need a valid API key.
+ * Every API key has a specific amount of quota. Each request increases the usage. If usage exceeds quota, no more requests can be made until usage is resetted again.
+ * This route takes the action that you want to execute and checks if you are allowed to do that. If so, usage increases.
+ * This method will be used internally for every action that you call via "normal" requests such as retrieving soc data and is not intended to be used from outside.
+ * 
+ * @apiParam  {String} key the API-Key to use
+ * 
+ * @apiParam {Object} referer the details about the current action to be executed
+ * @apiParam {String} referer.method the http method from the action
+ * @apiParam {String} referer.path the URL / path from the action
+ * @apiParam {String} referer.akey the AKey (scope) to be used within the action
+ * @apiParam {Object} [referer.params] Optional params object containing keys and values, e.g. {":id": "123"}
+ * 
+ * @apiSuccess {Number} quota The amount of requests allowed in a month
+ * @apiSuccess {Number} usage The amount of already used quota
+ * 
+ * @apiSuccessExample Authorised request with increased usage
+ *  HTTP/1.1 200 OK
+ *  {
+ *      "quota": "100",
+ *      "usage": "12"
+ *  }
+ * 
+ * @apiErrorExample {json} Key not found
+ *  HTTP/1.1 404 Not Found
+ *  {
+ *      "code": 404,
+ *      "message": "Authorization key invalid or not found. Ensure to send a Bearer Token within Authorization header or within URL if applicable"
+ *  }
+ * @apiErrorExample {json} Forbidden
+ *  HTTP/1.1 404 Not Found
+ *  {
+ *      "code": 403,
+ *      "message": "You are not permitted to execute this action"
+ *  }
+ */
 router.post('/:key', attachKey, authorizationController.useKey);
 
 module.exports = router;
